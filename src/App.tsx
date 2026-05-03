045df1e0f71977ebaf98a09f0d5988b680158063
oswin902m <1665434994@qq.com>
2026-05-03 14:52:50 +0800

    prime-react setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from 'primereact/button';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="card flex justify-content-center">
+      <Button label="Check" icon="pi pi-check" />
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
+import 'primeicons/primeicons.css';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './index.css';
 import App from './App.tsx';
+import './index.css';
 
+// https://primereact.org/installation/
+// https://primereact.org/icons/
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </StrictMode>,
 );
